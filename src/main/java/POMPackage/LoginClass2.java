d677d55c37baed4ec8a7e6eb613971ce7ade9598
Sanjay Sangle <sanjay.sangle@ril.com>
2025-03-22 12:13:53 +0530

    changes modify

--- src/main/java/POMPackage/LoginClass2.java
+++ src/main/java/POMPackage/LoginClass2.java
@@ -3,6 +3,7 @@ package POMPackage;
 public class LoginClass2 {
 
 	public static void main(String[] args) {
+		System.out.println("sanjaysangle");
 		
 	}
 
--- src/main/java/POMPackage/changePassword.java
+++ src/main/java/POMPackage/changePassword.java
@@ -3,7 +3,11 @@ package POMPackage;
 public class changePassword {
 
 	public static void main(String[] args) {
-		System.out.println("change Passwod sanjay");
+		System.out.println("change Passwod vijay");
+		System.out.println("change Passwod vijay2");
+		System.out.println("change Passwod vijay3");
+
+
 
 	}
 
