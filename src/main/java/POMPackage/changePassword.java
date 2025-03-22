b29d5cce1f9eef05f0d2b504945fe68a82a5b89f
Sanjay Sangle <sanjay.sangle@ril.com>
2025-03-22 12:17:31 +0530

    forgot pass

--- src/main/java/POMPackage/changePassword.java
+++ src/main/java/POMPackage/changePassword.java
@@ -3,6 +3,7 @@ package POMPackage;
 public class changePassword {
 
 	public static void main(String[] args) {
+		System.out.println("change Passwod dhiraj");
 		System.out.println("change Passwod sanjay");
 
 	}
